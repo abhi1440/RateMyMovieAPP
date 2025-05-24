aa9ac0a026251dcd993002c76d91866716ff1898
Samarth agarwal <160070709+abhi1440@users.noreply.github.com>
2025-05-24 14:39:55 +0530

    Update constants.js

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,6 +1,11 @@
 // src/redux/constants.js
-export const BASE_URL = '/api/v1/';
-export const USERS_URL = '/api/v1/users';
-export const GENRE_URL = "/api/v1/genre";
-export const MOVIE_URL = '/api/v1/movies';
-export const UPLOAD_URL = '/api/v1/upload';
+// src/redux/constants.js
+
+// You can set this dynamically based on environment variable
+const BASE_API_URL = import.meta.env.VITE_API_URL || '';
+
+export const BASE_URL = BASE_API_URL + '/api/v1/';
+export const USERS_URL = BASE_API_URL + '/api/v1/users';
+export const GENRE_URL = BASE_API_URL + "/api/v1/genre";
+export const MOVIE_URL = BASE_API_URL + '/api/v1/movies';
+export const UPLOAD_URL = BASE_API_URL + '/api/v1/upload';
